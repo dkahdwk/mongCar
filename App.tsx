602d7202fbe4daad043539f3d47696b54486fa72
이명제 <gkahk@naver.com>
2022-11-15 15:59:11 +0900

    [Feat] Add global StatusBar in App.tsx

--- App.tsx
+++ App.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import NavController from 'navigations/NavController';
 import { ThemeProvider } from 'styled-components';
 import { MobxProvider } from 'stores/RootStore';
+import { StatusBar } from 'react-native';
 import theme from 'styles/theme';
 
 const App = () => {
   return (
     <MobxProvider>
       <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="dark-content"
+          /**
+           * translucent: Android 에서만 동작한다. (투명 배경의 목적)
+           * backgroundColor: Android 에서만 동작한다. (투명 배경의 목적)
+           * */
+          backgroundColor={'transparent'}
+          translucent
+        />
         <NavController />
       </ThemeProvider>
     </MobxProvider>
