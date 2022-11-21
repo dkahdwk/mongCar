8baed4d9c9242dd9f948bff9eeb8666f02422fcd
이명제 <gkahk@naver.com>
2022-11-21 09:57:42 +0900

    [Chore] Add moment

--- src/stores/RootStoreObject.tsx
+++ src/stores/RootStoreObject.tsx
@@ -1,9 +1,11 @@
-import MapStore from 'stores/MapStore';
+import CommonStore from 'stores/CommonStore';
 import AuthStore from 'stores/AuthStore';
+import MapStore from 'stores/MapStore';
 import UIStore from 'stores/UIStore';
 
 // eslint-disable-next-line import/prefer-default-export
 export const RootStoreObject = {
+  commonStore: new CommonStore(),
   authStore: new AuthStore(),
   mapStore: new MapStore(),
   uiStore: new UIStore(),
